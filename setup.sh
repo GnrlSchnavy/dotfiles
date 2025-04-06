4a227f489e00ad8a482208bdb16add36484a353b
Yvan <yvanstemmerik@gmail.com>
2025-04-06 22:30:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
   sh -s -- install
 
+sudo mkdir -p /etc/nix-darwin
+sudo chown $(id -nu):$(id -ng) /etc/nix-darwin
+cd /etc/nix-darwin
+
+# To use Nixpkgs unstable:
+nix flake init -t nix-darwin/master
+# To use Nixpkgs 24.11:
+nix flake init -t nix-darwin/nix-darwin-24.11
+
+sed -i '' "s/simple/$(scutil --get LocalHostName)/" flake.nix
+
 # To use Nixpkgs unstable:
 # nix run nix-darwin/master#darwin-rebuild -- switch
 # To use Nixpkgs 24.11:
