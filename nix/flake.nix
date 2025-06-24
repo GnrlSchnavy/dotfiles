ac88a4bf3e38b55e9e36ceed28ff3e38572d30e9
Yvan <yvanstemmerik@gmail.com>
2025-06-24 20:57:50 +0200

    update

--- nix/flake.nix
+++ nix/flake.nix
@@ -86,7 +86,9 @@
               "omnissa-horizon-client"
             ];
             brews = [
+       "sshpass"
 	      "kubeseal"
+	      "tree"
 	      "fluxcd/tap/flux"
 	      "pgloader"
 	      "p7zip"
